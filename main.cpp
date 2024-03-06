bf4ef88479207f27c4608c06bb7b21f483446130
Matteo Cardinaletti <matteo.cardinaletti03@gmail.com>
2024-03-06 16:15:01 +0100

    Testing the Lexer

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
+#include "./src/front/Lexer.h"
+
 int main(void) {
-    std::cout << "Hello, World\n";
+    Lexer l = Lexer();
+    // TODO: testing
+    auto file = "./example/test.xt";
+    auto v = l.lex_file(file);
+
+    std::cout << "len(tokens) = " << v.size() << std::endl; 
+
+    for (auto tkn : v) {
+        std::cout << tkn.text << std::endl;
+    }
     return 0;
 }
